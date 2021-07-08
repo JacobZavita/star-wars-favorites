e2d18340b20fe21c2cc12efd094131365dcbf912
Jacob Zavita <jacobzavita@gmail.com>
2021-07-08 16:43:37 -0700

    appbar

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -30,9 +30,9 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Star Wars Favorites
           </Typography>
-          <Button color="inherit">Login</Button>
+          {/* <Button color="inherit">Login</Button> */}
         </Toolbar>
       </AppBar>
     </div>
