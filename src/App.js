f91a949efec90fcd8b96da6534c13c3afe84675c
Jacob Zavita <jacobzavita@gmail.com>
2021-07-08 14:28:00 -0700

    add appbar and card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react'
+// import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Search from './pages/Search'
+import Saved from './pages/Saved'
+import Appbar from './components/Appbar'
+import MediaCard from './components/Card' 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Appbar />
+        <MediaCard />
+        <Switch>
+          <Route exact path='/'>
+            <Search />
+          </Route>
+          <Route path='/saved'>
+            <Saved />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Search/Search.js
@@ -0,0 +1,16 @@
+// import {
+//   Container,
+//   Row,
+//   Col,
+//   Jumbotron
+// } from 'reactstrap'
+// import Form from '../../components/Form'
+// import Display from '../../components/Display'
+
+const Search = props => {
+  return (
+    <h1>Search Page</h1>
+  )
+}
+
+export default Search
