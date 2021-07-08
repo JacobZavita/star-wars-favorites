0ada7157d7aef82a88acf2d7761976b5d363e549
Jacob Zavita <jacobzavita@gmail.com>
2021-07-08 16:39:27 -0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-// import { useState } from 'react'
-// import axios from 'axios'
+import { useState } from 'react'
+import axios from 'axios'
+
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom'
+
 import Search from './pages/Search'
 import Saved from './pages/Saved'
 import Appbar from './components/Appbar'
 import MediaCard from './components/Card' 
 
 const App = () => {
+  const [charState, setCharState] = useState({
+    search: '',
+    char: {},
+    chars: []
+  })
+
+  const handleInputChange = ({ target }) => {
+    setCharState({ ...charState, [target.name]: target.value })
+  }
+
+  const handleSearchChar = () => {
+    const chars = [...charState.chars]
+    chars.push(charState.char)
+    setCharState({ ...charState, chars, char: {} })
+  }
+
+  const handleSaveChar = () => {
+    const chars = [...charState.chars]
+    chars.push(charState.char)
+    setCharState({ ...charState, chars, char: {} })
+  }
+
   return (
     <Router>
       <div>
@@ -18,10 +42,18 @@ const App = () => {
         <MediaCard />
         <Switch>
           <Route exact path='/'>
-            <Search />
+            <Search 
+              search={charState.search}
+              char={charState.char}
+              handleSaveChar={handleSaveChar}
+              handleInputChange={handleInputChange}
+              handleSearchChar={handleSearchChar}
+            />
           </Route>
           <Route path='/saved'>
-            <Saved />
+            <Saved 
+              chars={charState.chars}
+            />
           </Route>
         </Switch>
       </div>
