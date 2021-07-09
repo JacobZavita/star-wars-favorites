eb84538d7d38f3f906e024fea9a70ec80e4c070e
Jacob Zavita <jacobzavita@gmail.com>
2021-07-08 17:41:05 -0700

    add button to form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import ContainedButtons from '../Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,6 +18,7 @@ export default function BasicTextFields() {
   return (
     <form className={classes.root} noValidate autoComplete="off">
       <TextField id="outlined-basic" label="Search A Character" variant="outlined" />
+      <ContainedButtons />
     </form>
   );
 }
